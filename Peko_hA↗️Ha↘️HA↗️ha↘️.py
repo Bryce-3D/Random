17673ebc02914f4248a6918e936a39631e6ab302
Bryce 3D <94155427+Bryce-3D@users.noreply.github.com>
2022-12-05 11:49:46 +0800

    Added comments

--- Peko_hA↗️Ha↘️HA↗️ha↘️.py
+++ Peko_hA↗️Ha↘️HA↗️ha↘️.py
@@ -6,15 +6,18 @@ will format the text to be *italic*, **bold**, or ***both***.
 
 from random import *
 
+#ha[i][j] = the ith ha capitalization nested between j pairs of asterisks
 ha = [['ha','hA','Ha','HA']]
 for i in range(3):
     ha.append(['*' + i + '*' for i in ha[-1]])
 arr = ['↗️', '↘️']
 
+#Number of ha's is in the range [min_ha:max_ha]
 min_ha, max_ha = [int(i) for i in input().split()]
 n = randrange(min_ha, max_ha)
 s = []
 
+#Randomly put together the selected number of ha's
 for i in range(n):
     s.append( choice(choice(ha)) )
     s.append( choice(arr) )
